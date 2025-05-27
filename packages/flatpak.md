9f989423e465b03a57de83fa85ec877a19bd20f8
cypheroxide <cypheroxide@cyberservices.com>
2025-05-27 18:11:53 -0500

    feat: Add Flatpak package list and installation script

---
+++ packages/flatpak.md
@@ -0,0 +1,10 @@
+# Flatpak Packages
+
+## Currently Installed Packages
+- Discord (com.discordapp.Discord) - v0.0.90
+- Alpaca (com.jeffser.Alpaca) - v6.0.3
+- OBS Studio (com.obsproject.Studio) - v31.0.3
+- Plex Desktop (tv.plex.PlexDesktop) - v1.108.1
+
+## Installation Script
+To install these packages, run the install-flatpaks.sh script in this directory.
---
+++ packages/install-flatpaks.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Add Flathub repository if not already added
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Install packages
+flatpak install flathub com.discordapp.Discord -y
+flatpak install flathub com.jeffser.Alpaca -y
+flatpak install flathub com.obsproject.Studio -y
+flatpak install flathub tv.plex.PlexDesktop -y
